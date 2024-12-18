d4f90d641e47d233b75f8c5e15ed0fca3704ff0d
ericvenom99 <prevenakhtar@mail.com>
2024-12-18 15:12:32 +0100

    created the useUser.tsx

---
+++ Social/src/hooks/useUser.tsx
@@ -0,0 +1,24 @@
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { toast } from 'react-toastify'; // Assuming you're using react-toastify for notifications
+
+const registerUser = async (userData) => {
+  const response = await axios.post("http://localhost:5000/api/auth/register", userData);
+  return response.data;
+};
+
+export const useCreateUserAccountMutation = () => {
+  return useMutation({
+    mutationFn: (user) => registerUser(user),
+    onSuccess: () => {
+      toast.success('User registered successfully!');
+      // Additional success handling can be added here
+    },
+    onError: (error) => {
+      console.error("Registration failed:", error);
+      const errorMessage = error.response?.data?.message || 'There was an error registering the user!';
+      toast.error(errorMessage);
+      // Additional error handling can be added here
+    },
+  });
+};
