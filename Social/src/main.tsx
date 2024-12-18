a31e5dfe2ac196405cc8cb31a4c8601ed7e8bdfb
ericvenom99 <prevenakhtar@mail.com>
2024-12-18 15:15:41 +0100

    updated files

--- Social/src/main.tsx
+++ Social/src/main.tsx
@@ -1,8 +1,14 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <App />
+    <BrowserRouter future={{ v7_relativeSplatPath: true }}>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </BrowserRouter>
-)
+);
